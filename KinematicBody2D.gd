cafb1de32253205565b6de4cb1276d80f6f2393c
achmaddzaki <achmaddzaki95@gmail.com>
2019-10-23 21:54:30 +0700

    # Add Player Animation # Add Jumping function

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -7,8 +7,11 @@ var currentposition = Vector2()
 func _physics_process(delta):
 	currentposition.y += gravity
 	if(is_on_floor()):
+		$AnimatedSprite.play("Run")
 		if(Input.is_action_pressed("ui_up")):
 			currentposition.y -= jump_high
+	else:
+		$AnimatedSprite.play("Jump")
 	
 	currentposition = move_and_slide(currentposition, Vector2.UP)
 	pass
